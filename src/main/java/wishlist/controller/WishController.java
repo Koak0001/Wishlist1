8720e9583f7bd9f81510e3bf3d314b5cc6b6a0a2
Kodjo Akator <koak0001@stud.kea.dk>
2024-04-14 12:51:54 +0200

    resolves #43

--- src/main/java/wishlist/controller/WishController.java
+++ src/main/java/wishlist/controller/WishController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import wishlist.model.ItemList;
 import wishlist.service.WishService;
@@ -35,5 +36,10 @@ public String createList(Model model){return "create";}
         ItemList itemlist = wishService.getItemListByName(listName);
         model.addAttribute("itemlist", itemlist);
         return "itemlist";
+    }
+    @PostMapping("wishlists/delete/{listName}")
+    public String delete(@PathVariable String listName, Model model) {
+        wishService.deleteItemList(listName);
+        return "redirect:/wishlist/wishlists";
     }
 }
