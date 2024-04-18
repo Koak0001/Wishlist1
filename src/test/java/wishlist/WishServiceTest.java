976aba002af9539920dfdc888ac05ae4665b858a
Kodjo Akator <koak0001@stud.kea.dk>
2024-04-18 20:58:57 +0200

    Struggling!

---
+++ src/test/java/wishlist/WishServiceTest.java
@@ -0,0 +1,23 @@
+package wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import wishlist.model.ItemList;
+import wishlist.repository.WishRepository;
+import wishlist.service.WishService;
+
+class WishServiceTest {
+
+    @Test
+    void testAddItemList() {
+        // Arrange
+        WishRepository mockRepository = Mockito.mock(WishRepository.class);
+        WishService wishService = new WishService(mockRepository);
+        ItemList itemList = new ItemList("Test ItemList");
+
+
+        wishService.addItemList(itemList);
+
+        Mockito.verify(mockRepository).addNewItemList(itemList);
+    }
+}
